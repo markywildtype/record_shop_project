46c293d1b5730a8fc0333a46552014c442d859e5
Mark Blanford <markytorture@hotmail.com>
2017-12-15 14:43:36 +0000

    record_shop database and artist/album tables created

---
+++ db/record_shop.sql
@@ -0,0 +1,16 @@
+DROP TABLE IF EXISTS albums;
+DROP TABLE IF EXISTS artists;
+
+CREATE TABLE artists (
+  id SERIAL8 PRIMARY KEY,
+  name VARCHAR(255),
+  type VARCHAR(255)
+);
+
+CREATE TABLE albums (
+  id SERIAL8 PRIMARY KEY,
+  artist_id SERIAL8 REFERENCES artists(id),
+  title VARCHAR(255),
+  release_date VARCHAR(255),
+  stock INT4
+);
--- models/album.rb
+++ models/album.rb
@@ -5,6 +5,7 @@ class Album
 
   def initialize(options)
     @id = options['id'].to_i
+    @artist_id = options['artist_id'].to_i
     @title = options['title']
     @release_date = options['release_date']
     @stock = options['stock'].to_i
